5f260ed0416c5c69abcc51e57dc5c352c3241eb9
Michael Kan <michaelkan@Michaels-MacBook-Pro.local>
2023-07-20 23:40:20 +0900

    Project 22

---
+++ Project22-iBeacon/Project22-iBeacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22-iBeacon
+//
+//  Created by Michael Kan on 2023/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
