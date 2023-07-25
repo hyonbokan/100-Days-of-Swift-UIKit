02b208c4184b1125a38616b4720ebbbb7edc2aff
Michael Kan <michaelkan@Michaels-MacBook-Pro.local>
2023-07-25 21:08:08 +0900

    Project 24

---
+++ Project24.playground/Contents.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+
+let name = "Taylor"
+
+for letter in name {
+    print("Letters in name: \(letter)")
+}
+
+//The reason for this is that letters in a string aren’t just a series of alphabetical characters – they can contain accents such as á, é, í, ó, or ú, they can contain combining marks that generate wholly new characters by building up symbols, or they can even be emoji.
+
+let letter = name[name.index(name.startIndex, offsetBy: 3)] //forth character in Taylor
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+let letter2 = name[3]
+
+let password = "12345"
+password.hasPrefix("123")
+password.hasSuffix("456")
+
+extension String {
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+    
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
+    
+}
+
+
+let weather = "it's going to rain"
+print(weather.capitalized)
+
+
+extension String {
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+
+let input = " Swift is like Objective-C without the C"
+input.contains("Swift")
+
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+extension String {
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+input.containsAny(of: languages)
+
+languages.contains(where: input.contains)
+
+let string = "This is a test string"
+
+//let attributes: [NSAttributedString.Key: Any] = [
+//    .foregroundColor: UIColor.white,
+//    .backgroundColor: UIColor.red,
+//    .font: UIFont.boldSystemFont(ofSize: 36)]
+
+
+//let attributedString = NSAttributedString(string: string, attributes: attributes)
+
+
+let attributedString = NSMutableAttributedString(string: string)
+
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 5, length: 2))
+
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 8, length: 1))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 10, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 15, length: 6))
+
+
+    
