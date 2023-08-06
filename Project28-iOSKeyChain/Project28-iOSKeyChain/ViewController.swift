11d9253abb657534ee118ee03cb33e14fb000626
hyonbokan <mkkanhb@gmail.com>
2023-08-07 00:39:37 +0900

    Project 28

--- Project28-iOSKeyChain/Project28-iOSKeyChain/ViewController.swift
+++ Project28-iOSKeyChain/Project28-iOSKeyChain/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var secret: UITextView!
     
+    var password = "123456"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveSecretMessage))
+        
+        print("Initial password: \(password)")
+        password = KeychainWrapper.standard.string(forKey: "Password") ?? "empty"
     }
 
     @IBAction func authenticateTapped(_ sender: Any) {
@@ -37,7 +43,12 @@ class ViewController: UIViewController {
                         //error
                         let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again", preferredStyle: .alert)
                         ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        // add ui action to trigger password alert
+                        ac.addAction(UIAlertAction(title: "Use password", style: .default, handler: self?.passwordAlert))
+                        // add ui alert action to set new password and save it in keychain
+                        ac.addAction(UIAlertAction(title: "New password", style: .default, handler: self?.newPasswordAlert))
                         self?.present(ac, animated: true)
+                        
                     }
                 }
                 
@@ -46,8 +57,40 @@ class ViewController: UIViewController {
             // no biometry
             let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
+            
         }
+    }
+    
+    func passwordAlert(action: UIAlertAction){
+        let ac = UIAlertController(title: "Input your password", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let checkPassword = UIAlertAction(title: "OK", style: .default) {
+            [weak self, weak ac] _ in
+            if ac?.textFields?[0].text == self?.password {
+                self?.unlockSecretMessage()
+            } else {
+                let errorAlert = UIAlertController(title: "Error", message: "The password is incorrect!", preferredStyle: .alert)
+                errorAlert.addAction(UIAlertAction(title: "OK", style: .default))
+                self?.present(errorAlert, animated: true)
+            }
+        }
+        ac.addAction(checkPassword)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func newPasswordAlert(action: UIAlertAction) {
+        let ac = UIAlertController(title: "Set new password", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let setNewPassword = UIAlertAction(title: "Set", style: .default){
+            [weak self, weak ac] _ in
+            let newPassword = ac?.textFields?[0].text
+            self?.password = newPassword ?? "none"
+            print("new password: \(self?.password)")
+        }
+        ac.addAction(setNewPassword)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
     }
     
     @objc func adjustForKeyboard(notification: Notification){
@@ -70,22 +113,23 @@ class ViewController: UIViewController {
     func unlockSecretMessage() {
         secret.isHidden = false
         title = "Secret text"
-        
+//        var savedPassword = password
 //        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
 //            secret.text = text
 //        }
         secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
-        
+        print("Unlock global password: \(password)")
     }
     
     @objc func saveSecretMessage() {
         guard secret.isHidden == false else { return }
-        
-        
+       
         KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        KeychainWrapper.standard.set(password, forKey: "Password")
         secret.resignFirstResponder()
         secret.isHidden = true
         title = "Nothing to see here"
+        print("Currently saved password: \(password)")
     }
     
 }
